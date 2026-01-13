4fdc9ddc7dbd0e279bd3f136285ca17a2492c19e
Fernando Arevalo <53315976+farevalo1008@users.noreply.github.com>
2026-01-13 14:36:03 -0400

    se agrega componente button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 
-import { COLORS, Spinner } from 'containers/core';
+
 import { useFonts } from 'expo-font';
-import { StatusBar } from 'expo-status-bar';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import { StyleSheet, Text, View, Button, ActivityIndicator, TextInput, Alert, TouchableOpacity } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button } from '@core';
 
 export default function App() {
     // Use `useFonts` only if you can't use the config plugin.
@@ -26,18 +26,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button title='Presioname' onPress={() => Alert.alert('Hola Mundo')}></Button>
-      <TouchableOpacity onPress={() => Alert.alert('Hola Mundo')}
-        style={{
-          backgroundColor: COLORS.primary,
-          padding:16,
-          borderRadius: 5,
-          marginTop: 10
-        }}>
-        <Text style={{color: COLORS.white, fontSize: 24 }}>Hola soy un boton custom</Text>
-      </TouchableOpacity>
-      <ActivityIndicator size='large' color='blue'/>
-      <TextInput style={{backgroundColor: 'pink', width: 300, padding:16 }}/>
+      <Button title='Hello' onPress={() => console.log('Hello')}/>
     </View>
   );
 }
---
+++ src/core/components/Button.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, TouchableOpacity,  } from 'react-native'
+import React from 'react'
+import { COLORS } from 'core/constants'
+
+interface ButtonProps{
+    title: string
+    onPress: ()=> void
+}
+
+export const Button = ({title, onPress}: ButtonProps) => {
+  return (
+        <TouchableOpacity style={styles.btn} onPress={onPress} >
+            <Text style={styles.txt}>{title}</Text>
+        </TouchableOpacity>
+  )
+}
+
+export default Button
+
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: COLORS.primary,
+    padding: 16,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  txt:{
+    color: COLORS.white, 
+    fontSize: 24 
+  }
+})
